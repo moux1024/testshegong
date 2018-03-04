9f6118b9e1a4b514a701bc34dd76a552f2668366
sunyuming <sunyuming@quarkioe.com>
2018-03-04 19:17:33 +0800

    2.5.8

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const imgFile = {};
 const date = document.querySelector('.date')
 const ifsw = Date.parse('2018/3/20')
+const bodyHeight = document.body.scrollHeight
 
 document.ontouchmove = function(event) {
 	event.preventDefault();
@@ -209,79 +210,81 @@ function dates(e) {
 
 //立即生成
 function generate() {
-	const set_name = document.querySelector("#name").value
-	const set_type = document.querySelector("#type").value
-	const set_date = (document.querySelector("#date").value).replace(/\-/g, "/")
-	const img = document.querySelector('.img').querySelectorAll('img').item(0)
-	if (img !== null) {
-		if (set_name.length > 0) {
-			if (set_type !== 'none') {
-				document.querySelector(".get_name").innerHTML = `我是${set_name}`
-				if (set_type == 'yes') {
-					if (set_date.length > 0) {
-						const get_date = parseInt((ifsw - Date.parse(set_date)) / (1000 * 60 * 60 * 24) + 1)
+	if (document.body.scrollHeight == bodyHeight) {
+		const set_name = document.querySelector("#name").value
+		const set_type = document.querySelector("#type").value
+		const set_date = (document.querySelector("#date").value).replace(/\-/g, "/")
+		const img = document.querySelector('.img').querySelectorAll('img').item(0)
+		if (img !== null) {
+			if (set_name.length > 0) {
+				if (set_type !== 'none') {
+					document.querySelector(".get_name").innerHTML = `我是${set_name}`
+					if (set_type == 'yes') {
+						if (set_date.length > 0) {
+							const get_date = parseInt((ifsw - Date.parse(set_date)) / (1000 * 60 * 60 * 24) + 1)
+							document.querySelector('.set').style.display = 'none'
+							document.querySelector('.get').style.display = 'block'
+							const yes_t = document.querySelectorAll('.yes_t')
+							for (let i = 0; i < yes_t.length; i++) {
+								yes_t[i].style.display = 'block'
+							}
+							document.querySelector('.yes_t1').style.opacity = '1'
+							document.querySelector('.call').style.display = 'none'
+							document.querySelector(".get_date").innerHTML = `今天是我加入社工第${get_date}天`
+							switch (get_date.toString().length) {
+								case 4:
+									document.querySelector(".qian").innerHTML = get_date.toString().charAt(0)
+									document.querySelector(".bai").innerHTML = get_date.toString().charAt(1)
+									document.querySelector(".shi").innerHTML = get_date.toString().charAt(2)
+									document.querySelector(".ge").innerHTML = get_date.toString().charAt(3)
+									break
+								case 3:
+									document.querySelector(".qian").innerHTML = 0
+									document.querySelector(".bai").innerHTML = get_date.toString().charAt(0)
+									document.querySelector(".shi").innerHTML = get_date.toString().charAt(1)
+									document.querySelector(".ge").innerHTML = get_date.toString().charAt(2)
+									break
+								case 2:
+									document.querySelector(".qian").innerHTML = 0
+									document.querySelector(".bai").innerHTML = 0
+									document.querySelector(".shi").innerHTML = get_date.toString().charAt(0)
+									document.querySelector(".ge").innerHTML = get_date.toString().charAt(1)
+									break
+								case 1:
+									document.querySelector(".qian").innerHTML = 0
+									document.querySelector(".bai").innerHTML = 0
+									document.querySelector(".shi").innerHTML = 0
+									document.querySelector(".ge").innerHTML = get_date.toString().charAt(0)
+									break
+							}
+							document.querySelector(".download").style.display = 'block'
+							convert2canvas()
+						} else {
+							document.querySelector('.erro').innerHTML = '请选择日期'
+							setTimeout("document.querySelector('.erro').innerHTML = ''", 2000)
+						}
+					} else {
 						document.querySelector('.set').style.display = 'none'
 						document.querySelector('.get').style.display = 'block'
 						const yes_t = document.querySelectorAll('.yes_t')
 						for (let i = 0; i < yes_t.length; i++) {
-							yes_t[i].style.display = 'block'
+							yes_t[i].style.display = 'none'
 						}
-						document.querySelector('.yes_t1').style.opacity = '1'
-						document.querySelector('.call').style.display = 'none'
-						document.querySelector(".get_date").innerHTML = `今天是我加入社工第${get_date}天`
-						switch (get_date.toString().length) {
-							case 4:
-								document.querySelector(".qian").innerHTML = get_date.toString().charAt(0)
-								document.querySelector(".bai").innerHTML = get_date.toString().charAt(1)
-								document.querySelector(".shi").innerHTML = get_date.toString().charAt(2)
-								document.querySelector(".ge").innerHTML = get_date.toString().charAt(3)
-								break
-							case 3:
-								document.querySelector(".qian").innerHTML = 0
-								document.querySelector(".bai").innerHTML = get_date.toString().charAt(0)
-								document.querySelector(".shi").innerHTML = get_date.toString().charAt(1)
-								document.querySelector(".ge").innerHTML = get_date.toString().charAt(2)
-								break
-							case 2:
-								document.querySelector(".qian").innerHTML = 0
-								document.querySelector(".bai").innerHTML = 0
-								document.querySelector(".shi").innerHTML = get_date.toString().charAt(0)
-								document.querySelector(".ge").innerHTML = get_date.toString().charAt(1)
-								break
-							case 1:
-								document.querySelector(".qian").innerHTML = 0
-								document.querySelector(".bai").innerHTML = 0
-								document.querySelector(".shi").innerHTML = 0
-								document.querySelector(".ge").innerHTML = get_date.toString().charAt(0)
-								break
-						}
+						document.querySelector('.text').style.marginTop = (15 / 32.5) + 'rem'
+						document.querySelector('.yes_t1').style.opacity = '0'
+						document.querySelector('.call').style.display = 'block'
 						document.querySelector(".download").style.display = 'block'
 						convert2canvas()
-					} else {
-						document.querySelector('.erro').innerHTML = '请选择日期'
-						setTimeout("document.querySelector('.erro').innerHTML = ''", 2000)
 					}
-				} else {
-					document.querySelector('.set').style.display = 'none'
-					document.querySelector('.get').style.display = 'block'
-					const yes_t = document.querySelectorAll('.yes_t')
-					for (let i = 0; i < yes_t.length; i++) {
-						yes_t[i].style.display = 'none'
-					}
-					document.querySelector('.text').style.marginTop = (15 / 32.5) + 'rem'
-					document.querySelector('.yes_t1').style.opacity = '0'
-					document.querySelector('.call').style.display = 'block'
-					document.querySelector(".download").style.display = 'block'
-					convert2canvas()
 				}
+			} else {
+				document.querySelector('.erro').innerHTML = '请输入姓名'
+				setTimeout("document.querySelector('.erro').innerHTML = ''", 2000)
 			}
 		} else {
-			document.querySelector('.erro').innerHTML = '请输入姓名'
+			document.querySelector('.erro').innerHTML = '请上传图片'
 			setTimeout("document.querySelector('.erro').innerHTML = ''", 2000)
 		}
-	} else {
-		document.querySelector('.erro').innerHTML = '请上传图片'
-		setTimeout("document.querySelector('.erro').innerHTML = ''", 2000)
 	}
 }
 
