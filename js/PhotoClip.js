f83304f6e9c8b489f7cdcd0d056c8433150d583f
moux1024 <403053463@qq.com>
2018-03-05 08:48:50 +0800

    3.0.2.1

--- js/PhotoClip.js
+++ js/PhotoClip.js
@@ -824,7 +824,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
                     'border': maskBorder,
                     'position': 'absolute',
                     'left': '50%',
-                    'top': '50%'
+                    'top': '0'
                 });
 
                 // 初始化视图容器
